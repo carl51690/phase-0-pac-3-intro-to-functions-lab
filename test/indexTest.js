65471e38938f27825b8055290208892a95b63178
Carl Adams <carl51690@gmail.com>
2023-09-11 09:52:31 -0400

    creates myfile.md

--- test/indexTest.js
+++ test/indexTest.js
@@ -3,7 +3,8 @@ require ( './root.js' );
 
 describe('shout(string)', function() {
   it('receives one argument and returns it in all caps', function() {
-    expect(shout('hello')).toEqual('HELLO');
+    expect(shout('hello'
+    )).toEqual('HELLO');
   })
 })
 
